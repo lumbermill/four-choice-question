ed428569bddeec27497e7527c5f2d8fd4ee73d9e
Yosei Ito <y-itou@lumber-mill.co.jp>
2018-10-15 11:46:36 +0900

    Initial commit(moved from lm/toolkit/docs).

---
+++ README.md
@@ -0,0 +1,5 @@
+# four-chince-question
+画像を見て、4つの選択肢から答えを選ぶタイプのAndrondアプリです。
+
+- androndapp アプリ本体
+- assets/images クイズに使用する画像
---
+++ androidapp/app/src/main/java/net/lmlab/four_choice_question/four_choice_question/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.lmlab.four_choice_question.four_choice_question
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
